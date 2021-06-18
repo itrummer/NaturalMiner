74d0af5b2a64ed35f4e38dd6cf58c17007bad723
itrummer <immanuel.trummer@gmail.com>
2021-06-18 01:03:01 -0400

    Started expanding caching strategy

--- src/cp/query.py
+++ src/cp/query.py
@@ -3,8 +3,10 @@ Created on Jun 5, 2021
 
 @author: immanueltrummer
 '''
-import psycopg2
+from collections import Counter
 from cp.util import is_pred
+import psycopg2
+import time
 
 class AggQuery():
     """ Represents a simple aggregation query. """
@@ -43,7 +45,7 @@ class AggQuery():
 
 class AggCache():
     """ Cache of aggregate values. """
-    
+
     def __init__(self, connection):
         """ Initializes query cache.
         
@@ -54,8 +56,9 @@ class AggCache():
         self.prefix = 'pcache'
         self.max_cached = 100
         self.t_to_slot = {}
+        self.query_log = []
         self.clear_cache()
-        
+
     def clear_cache(self):
         """ Clears all cached relations. """
         self.next_slot = 0
@@ -64,19 +67,10 @@ class AggCache():
                 cache_tbl = self.prefix + str(i)
                 sql = f'drop table if exists {cache_tbl}'
                 cursor.execute(sql)
-        
-    def is_cached(self, query):
-        """ Checks if result of query is cached. 
-        
-        Args:
-            query: check for result of this query
-            
-        Returns:
-            Boolean flag, true iff result is cached
-        """
-        template = query.template()
-        return True if template in self.t_to_slot else False
-        
+
+    def estimate_cardinality(self, template):
+        pass
+
     def get_result(self, query):
         """ Retrieve result from cache. 
         
@@ -92,15 +86,32 @@ class AggCache():
         
         p_parts = [f"{c}='{v}'" for c, v in query.eq_preds]
         w_clause = ' and '.join(p_parts)
-        sql = f'select rel_avg from {table} where {w_clause}'
+        sql = f'select case when cmp_c = 0 or s = 0 then NULL '\
+            f'else (cmp_s/cmp_c)/(s/c) end as rel_avg ' \
+            f'from {table} where {w_clause}'
         
         with self.connection.cursor() as cursor:
+            start_s = time.time()
             cursor.execute(sql)
+            print(f'Get time: {time.time() - start_s} seconds')
             if cursor.rowcount == 0:
                 return None
             else:
                 return cursor.fetchone()[0]
-    
+
+    def is_cached(self, query):
+        """ Checks if result of query is cached. 
+        
+        Args:
+            query: check for result of this query
+            
+        Returns:
+            Boolean flag, true iff result is cached
+        """
+        self.query_log.append(query)
+        template = query.template()
+        return True if template in self.t_to_slot else False
+
     def put_results(self, template):
         """ Generates and caches results for query template.
         
@@ -110,7 +121,6 @@ class AggCache():
         table, pred_cols, cmp_pred, agg_col = template
         cache_tbl = self.prefix + str(self.next_slot)
         q_parts = [f'create unlogged table {cache_tbl} as (']
-        q_parts += ['with sums as (']
         
         s_parts = [f'select sum({agg_col}) as s, count(*) as c']
         s_parts += [f'sum(case when {cmp_pred} then {agg_col} else 0 end) as cmp_s']
@@ -121,17 +131,110 @@ class AggCache():
         q_parts += [f' from {table}']
         if pred_cols:
             q_parts += [' group by ' + ', '.join(pred_cols)]
-        q_parts += [') select case when cmp_c = 0 or s = 0 then NULL ']
-        q_parts += ['else (cmp_s/cmp_c)/(s/c) end as rel_avg, * from sums)']
+        q_parts += [')']
         
         sql = ' '.join(q_parts)
         print(f'About to fill cache with SQL "{sql}"')
         with self.connection.cursor() as cursor:
+            start_s = time.time()
             cursor.execute(sql)
+            print(f'Put time: {time.time() - start_s} seconds')
         
         self.t_to_slot[template] = self.next_slot
         self.next_slot += 1
 
+    def _merge_templates(self, t_1, t_2):
+        """ Merge templates into a new template.
+        
+        Args:
+            t_1: first template to merge
+            t_2: second template to merge
+            
+        Returns:
+            template combining columns if compatible, otherwise None
+        """
+        table_1, col_1, pred_1, agg_1 = t_1
+        table_2, col_2, pred_2, agg_2 = t_2
+        if table_1 == table_2 and pred_1 == pred_2 and agg_1 == agg_2:
+            merged_cols = set()
+            merged_cols.update(col_1)
+            merged_cols.update(col_2)
+            return table_1, merged_cols, pred_1, agg_1
+        else:
+            return None
+
+    def _candidate_views(self):
+        """ Selects candidate templates for which to generate results. 
+        
+        Returns:
+            set of query templates representing candidates
+        """
+        t_counts = Counter()
+        for q in self.query_log:            
+            t = q.template()
+            t_counts.update(t)
+            
+        candidates = set(t_counts.most_common(10))
+        for _ in range(3):
+            expanded = set()
+            for t_1 in candidates:
+                for t_2 in candidates:
+                    t_m = self._merge_templates(t_1, t_2)
+                    expanded.add(t_m)
+            candidates.update(expanded)
+        
+        return candidates
+
+    def _views_to_add(self, candidates):
+        """ Select most interesting views to add.
+        
+        Args:
+            candidates: select among those views
+            
+        Returns:
+            best views to add
+        """
+        pass
+    
+    def _views_to_delete(self, to_add):
+        """ Select least interesting views to delete. 
+        
+        Args:
+            to_add: views to add
+            
+        Returns:
+            best views to delete
+        """
+        pass
+    
+    def _order_updates(self, to_add, to_delete):
+        """ Order additions and deletions to minimize cost. 
+        
+        Args:
+            to_add: views to be added
+            to_delete: views to be deleted
+            
+        Returns:
+            list of plan steps (action-template pairs)
+        """
+        pass
+
+    def _execute_plan(self, plan):
+        """ Execute a plan to update cache.
+        
+        Args:
+            plan: list of plan steps (action-template pairs)
+        """
+        pass
+
+    def update(self):
+        """ Updates cache content for maximum efficiency. """
+        candidates = self._candidate_views()
+        v_add = self._views_to_add(candidates)
+        v_del = self._views_to_delete(v_add)
+        plan = self._order_updates(v_add, v_del)
+        self._execute_plan(plan)
+        self.query_log.clear()
 
 class QueryEngine():
     """ Processes queries distinguishing entities from others. """
